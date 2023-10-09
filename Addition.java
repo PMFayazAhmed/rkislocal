930655e6d2683c4efcf5a4a7b9e99901b6e64d66
Fayaz <fana.fayaz@gmail.com>
2023-10-09 07:12:09 +0530

    Added two more operations

--- Addition.java
+++ Addition.java
@@ -2,11 +2,17 @@ class Addition
 {
 	public static void main(String args[])
 	{
-		int a,b,sum;
-		a=10;
-		b=20;
+		int a,b,sum,prod,subtraction;
+		a=50;
+		b=80;
 		sum=a+b;
 		System.out.println("The sum value is : "+sum);
+		prod=a*b;
+		System.out.println("The Product of two number is "+prod);
+		subraction=a-b;
+		System.out.println("The suvtraction result is "+subtraction);
+
+
 	}
 }
 
